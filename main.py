cb5381ca57e41e3f5b1f999d7d65989905a10b13
aanshuvi <shahaanshuvi@gmail.com>
2024-07-01 15:27:27 +0530

    final-cookie-clicker

--- main.py
+++ main.py
@@ -14,20 +14,34 @@ cookie = driver.find_element(By.ID,value="cookie")
 click_cookie = True
 items = driver.find_elements(by=By.CSS_SELECTOR, value="#store div")
 item_ids = [item.get_attribute("id") for item in items]
-five_min = time.time() + 60*5
-
-
+finish_time = time.time() + 60
 
 while click_cookie:    
     cookie.click()
 
     if time.time() > start_time:
         prices = [int(price.text.split("- ")[1].strip().replace(",", "")) for price in driver.find_elements(by=By.CSS_SELECTOR, value="#store b")[:-1]]
-        money = int(driver.find_element(By.ID,value="money").text.replace(",", ""))
-    cookie_upgrades = {}
-    for n in range(len(prices)):
-        cookie_upgrades[prices[n]] = item_ids[n]
+        money = int(driver.find_element(By.ID,value="money").text.replace(",", "").replace(" ",""))
+        cookie_upgrades = {}
+        for n in range(len(prices)):
+            cookie_upgrades[prices[n]] = item_ids[n]
 
+        affordable_upgrades = {}
+        for cost, id in cookie_upgrades.items():
+            if money > cost:
+                affordable_upgrades[cost] = id
 
+        if affordable_upgrades:
+            highest_price_affordable_upgrade = max(affordable_upgrades)
+            to_purchase_id = affordable_upgrades[highest_price_affordable_upgrade]
+
+            driver.find_element(by=By.ID, value=to_purchase_id).click()
+
+        start_time = time.time() + 5
+
+    if time.time() > finish_time:
+        cookie_per_s = driver.find_element(by=By.ID, value="cps").text
+        print(cookie_per_s)
+        click_cookie = False
 
 driver.quit()
